feb73d9d77dbfdc3a12c8415356242997ec46ea4
nacchogonza <nachomgonzalez93@gmail.com>
2020-06-08 14:32:47 -0300

    Archivos Servidor

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const authController = require('../controllers/authController');
+const auth = require('../middleware/auth');
+
+// Autentica Usuario / Inicia Sesion
+// api/auth
+router.post('/', 
+    authController.autenticarUsuario
+);
+
+// Obtiene el usuario autenticado
+router.get('/',
+    auth,
+    authController.usuarioAutenticado
+);
+
+module.exports = router;
