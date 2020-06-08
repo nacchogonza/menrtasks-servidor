0dc274124218fe83581334ff39d6ed0939b13f16
nacchogonza <nachomgonzalez93@gmail.com>
2020-06-08 16:23:25 -0300

    ajustes deploy3

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //puerto de la app
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -24,7 +24,7 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
-    console.log(`El servidor está funcionando en el puerto ${port}`)
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`)
 })
 //
